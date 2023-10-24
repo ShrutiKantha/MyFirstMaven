5e09c99ec0f0b0b35bbb7a1095aec14fe8c185d9
shrutikantha <kanthashruti@gmail.com>
2023-10-24 14:30:49 +0530

    Demo for Team

---
+++ src/main/java/com/app/demo/MyFirstMaven/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.MyFirstMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
